eb288531089986a3237d248c8b623175c4be0ee3
Paul Gearon <gearon@computer.org>
2013-05-21 01:16:53 -0400

    Initial data conversion. Type promotion not yet done, so multi-typed attributes can't be loaded yet

--- src/kiara/data.clj
+++ src/kiara/data.clj
@@ -3,9 +3,10 @@
   (:require [datomic.api :refer [q] :as d]
             [crg.turtle.parser :as p])
   (:import [java.util Date UUID Map]
-            [java.net URI]
-            [java.math BigDecimal BigInteger]
-            [datomic Peer]))
+           [java.net URI]
+           [java.math BigDecimal BigInteger]
+           [crg.turtle.parser BlankNode Literal]
+           [datomic Peer]))
 
 (defn namespace-data
   "Converts a namespace map into transaction data"
@@ -13,43 +14,72 @@
   (letfn [(prefix-entity [[prefix namespace]]
             {:db/id (Peer/tempid :db.part/user)
              :k/prefix prefix
-             :k/namespace namespace})])
-    (map prefix-entity prefixes))
+             :k/namespace namespace})]
+    (map prefix-entity prefixes)))
 
-(defn encode-object
-  "Converts an object into Object data for Datomic. A database value
-  is supplied to allow schema lookups.
-  Simple objects are returned as native data. Other objects get
-  structured data. Returns a pair containing the Datomic value object
-  along with a possible seq of other data that represents the object."
-  [db o]
-  ;; TODO - return the appropriate data
-  [o nil]
-  )
+(defn encode-blank [{:keys [id]} bmap]
+  (if-let [b (bmap id)]
+    [b bmap nil]
+    (let [b (Peer/tempid :db.part/user)]
+      [b (assoc bmap id b) nil])))
 
-;; TODO - check if upsert will work in a single transaction (suspect not).
-;; if not, then merge entities the same way that schema does
+(defprotocol ObjectEncodable
+  (object-encode [db e bmap as] "Encodes a node into a structure suitable for Datomic"))
+
+(defprotocol SubjectEncodable
+  (subject-encode [e bmap] "Encodes a node into a structure suitable for Datomic"))
+
+(extend-protocol ObjectEncodable
+  Literal
+  ;; TODO check the type works for t and lang, else exception.
+  ;; Default requires check for string type.
+  (object-encode [db {:keys [lex t lang]} bmap as]
+    (cond
+      t (let [node (Peer/tempid :db.part/user)]
+          [node bmap {:db/id node, :k/value-s lex, :k/datatype t}])
+      lang (let [node (Peer/tempid :db.part/user)]
+            [node bmap {:db/id node, :k/value-s lex :k/lang lang}])
+      :default [lex bmap nil]))
+  BlankNode
+  (object-encode [_ blank-node bmap as]
+    (encode-blank blank-node bmap))
+  Object
+  (object-encode [db o bmap as] [o bmap nil])
+  nil
+  ;; TODO check that the type works, else promote
+  (object-encode [db o bmap as] [o bmap nil]))
+
+(extend-protocol SubjectEncodable
+  Object
+  (subject-encode [o bmap] [o bmap nil])
+  BlankNode
+  (subject-encode [blank-node bmap]
+    (encode-blank blank-node bmap)))
+
 (defn triple-to-entity
   "Converts a triple into an RDF entity"
-  [db triple]
-  (let [subject (.getSubject triple)
-        predicate (.getPredicate triple)
-        object (.getObject triple)]
-    (when-not (keyword? predicate)
-      (throw (IllegalArgumentException. (str "Only QName predicates are supported: " predicate))))
-    (let [[object-data extra-data] (encode-object db object)
-          entity {:db/id (Peer/tempid :db.part/user)
-                  predicate object-data}]
-      (if (seq extra-data) (conj extra-data entity) [entity]))))
+  [db blank-map [subject predicate object]]
+  (when-not (keyword? predicate)
+    (throw (IllegalArgumentException. (str "Only QName predicates are supported: " predicate))))
+  (let [[object-data blank-map other] (object-encode db object blank-map predicate)
+        [subject-node blank-map _] (subject-encode subject blank-map)
+        entity {:db/id (Peer/tempid :db.part/user)
+                :k/id subject-node
+                predicate object-data}]
+    (if other
+      [[entity other] blank-map]
+      [[entity] blank-map])))
 
 (defn datomic-data
   "Creates Datomic entity data that represents triples that can be found on a stream.
-  A database is required to read the schema that will contain the triples.
+  A database is required to read the schema.
   Returns the data stream and the parser. The parser is needed, since it will not
   contain the prefix map until the data stream has been processed."
   [database i]
   (let [parser (p/create-parser i)
         triples (p/get-triples parser)
-        create-entity (partial triple-to-entity database)]
-    [(mapcat create-entity triples) parser]))
+        add-entity (fn [[s m] triple]
+                     (let [[entities new-map] (triple-to-entity database m triple)]
+                       [(concat s entities) new-map]))]
+    [(first (reduce add-entity [[] {}] triples)) parser]))
 
