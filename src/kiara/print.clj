b3465092c0a931a6c7de3ac0819026092d8b8599
Paul Gearon <gearon@computer.org>
2013-05-21 01:17:21 -0400

    Printing out core data, schema, and data for input file

--- src/kiara/print.clj
+++ src/kiara/print.clj
@@ -1,5 +1,7 @@
 (ns kiara.print
-  (:require [kiara.schema :as schema]
+  (:require [kiara.core-schema :as cschema]
+            [kiara.schema :as schema]
+            [kiara.data :as data]
             [clojure.java.io :as io])
   (:gen-class))
 
@@ -9,5 +11,10 @@
   (when-not (seq args)
     (println "Usage: kiara.print <ttl.file>")
     (System/exit 1))
+  (prn "CORE:")
+  (prn cschema/core-attributes)
+  (println)
   (with-open [f (io/input-stream (first args))]
-    (prn (schema/datomic-schema f))))
+    (prn (schema/datomic-schema f)))
+  (with-open [f (io/input-stream (first args))]
+    (prn (first (data/datomic-data nil f)))))
