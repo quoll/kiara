0d337adae7b150f4cfd62483272fb69516f8425e
Paul Gearon <gearon@computer.org>
2013-05-21 00:48:21 -0400

    moved to new turtle parser, to avoid URIs

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.0"]
-                 [org.clojars.quoll/turtle "0.1.0"]
+                 [org.clojars.quoll/turtle "0.1.2"]
                  [com.datomic/datomic-free "0.8.3826"]]
   :main kiara.print)
